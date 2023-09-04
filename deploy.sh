0676c1f27929cc7ecb574ef4f474845991b7c5c2
M Iqbal Tawakal <mit.iqi@gmail.com>
2023-09-04 14:43:58 +0700

    added command to deploy to cloud run automatically

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-PROJECT_ID="mitochondrion-project-344303"
 REGION="asia-southeast2"
+PROJECT_ID="mitochondrion-project-344303"
+
 REPOSITORY="looker-llm"
 IMAGE="eva-looker-llm:latest"
+SERVICE="eva-looker-llm"
 
-docker build --tag=$REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE .
+IMAGE_URL=$REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE
+PORT=8501
+SERVICE_ACCOUNT="default@mitochondrion-project-344303.iam.gserviceaccount.com"
 
-docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE
+# 1st build locally
+docker build --tag=$IMAGE_URL .
+
+# 2nd push into repo
+docker push $IMAGE_URL
+
+# 3rd deploy to cloud run
+gcloud run deploy $SERVICE --image $IMAGE_URL --region $REGION \
+    --allow-unauthenticated --port $PORT --service-account $SERVICE_ACCOUNT
